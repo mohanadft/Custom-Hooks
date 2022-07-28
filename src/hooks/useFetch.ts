e8a5b7d0f81e1a15d0a6a11c1fbf022a24188494
mohanadft <mohanadfteha@gmail.com>
2022-07-28 16:28:16 +0300

    Adding useFetch Hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+const useFetch = (url: string) => {
+	const [data, setData] = useState(null)
+	const [loading, setLoading] = useState(false)
+
+	async function getData(url: string) {
+		try {
+			setLoading(true)
+			const response = await fetch(url)
+			const data = await response.json()
+			setData(data)
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		getData(url)
+	}, [url])
+	return { data, loading }
+}
+
+export default useFetch
