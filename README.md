6dae4ce09ecbcce3c5ce997d0e695dcd682017fc
mohanadft <mohanadfteha@gmail.com>
2022-07-29 19:03:24 +0300

    Adding useForm Hook

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 * *```useFetch```*
 * *```useLocalStorage```*
+* *```useForm```*
---
+++ src/hooks/useForm.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const useForm = () => {
+	const [state, setState] = useState({})
+
+	const handleChange = (e: Event) => {
+		setState(prevState => {
+			const target = e.target as HTMLInputElement
+			return { ...prevState, [target.name]: target.value }
+		})
+	}
+
+	return [state, handleChange]
+}
